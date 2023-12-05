5d80d2e9d5fbed895d7010b38799a46f927a33f1
James Parker <james@galois.com>
2023-12-05 10:47:02 -0500

    Bug?

--- crates/flux-tests/tests/pos/merge.rs
+++ crates/flux-tests/tests/pos/merge.rs
@@ -11,7 +11,7 @@ pub fn assert(b:bool) {
 }
 
 #[flux::sig(fn(xs:RVec<T>[@xl], ys:RVec<T>[@yl]) -> RVec<T>[xl + yl])]
-fn merge<T:Copy>(xs:RVec<T>, ys:RVec<T>) -> RVec<T> {
+fn concat<T:Copy>(xs:RVec<T>, ys:RVec<T>) -> RVec<T> {
     let mut r = RVec::new();
     let mut i = 0;
     while i < xs.len() {
@@ -22,6 +22,31 @@ fn merge<T:Copy>(xs:RVec<T>, ys:RVec<T>) -> RVec<T> {
     while i < ys.len() {
         r.push(ys[i]);
         i += 1;
+    }
+    r
+}
+
+#[flux::sig(fn(xs:RVec<T>[@xl], ys:RVec<T>[@yl]) -> RVec<T>[xl + yl])]
+fn merge<T:Copy + PartialOrd>(xs:RVec<T>, ys:RVec<T>) -> RVec<T> {
+    let mut r = RVec::new();
+    let mut i = 0;
+    let mut j = 0;
+    while i + j < xs.len() + ys.len() {
+        if i == xs.len() {
+            r.push(ys[j]);
+            j += 1;
+        } else if j == ys.len() {
+            r.push(xs[i]);
+            i += 1;
+        } else {
+            if xs[i] < ys[j] {
+                r.push(xs[i]);
+                i += 1;
+            } else {
+                r.push(ys[j]);
+                j += 1;
+            }
+        }
     }
     r
 }
